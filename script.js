557cf739ecf892c21b81ed3a5930177d2438b747
Eloise966 <13363735018@163.com>
2025-04-02 22:15:43 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ function showSection(id) {
   const sections = document.querySelectorAll('.section');
   sections.forEach(sec => {
     sec.classList.remove('visible');
-    sec.classList.add('hidden');
   });
   const target = document.getElementById(id);
   if (target) {
-    target.classList.remove('hidden');
     target.classList.add('visible');
   }
 }
-document.addEventListener('DOMContentLoaded', () => showSection('about'));
+document.addEventListener('DOMContentLoaded', () => showSection('home'));
